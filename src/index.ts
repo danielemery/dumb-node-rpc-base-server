9086cc7077b725a4d105ce2a3c9175c2b52b9c62
Daniel Emery <danielremery@gmail.com>
2020-10-25 21:25:04 +1030

    Add initial koa implementation

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import Koa from 'koa';
+import Router from '@koa/router';
+import Logger from '@danielemeryau/logger';
+import bodyParser from 'koa-bodyparser';
+
+export default abstract class DumbNodeRPCBaseServer {
+  private koa: Koa;
+  private router: Router;
+  private logger: Logger;
+  private port: number;
+
+  constructor(loggerName: string, port: number) {
+    this.koa = new Koa();
+    this.router = new Router();
+    this.logger = new Logger(loggerName);
+    this.port = port;
+  }
+
+  protected addRoute(route: string, service: (request: any) => Promise<any>) {
+    this.router.post(route, async (ctx) => {
+      this.logger.info(`Request ${route}`, ctx.request.body);
+      const result = await service(ctx.body);
+      ctx.response.status = 200;
+      ctx.response.body = result;
+    });
+  }
+
+  public listen() {
+    this.koa.use(bodyParser());
+    this.koa.use(this.router.routes());
+
+    this.logger.info(`Application listening on ${this.port}`);
+    this.koa.listen(this.port);
+  }
+}
---
+++ test/integration-test.ts
@@ -0,0 +1,16 @@
+import DumbNodeRPCBaseServer from '../src/index';
+
+class TestServer extends DumbNodeRPCBaseServer {
+  constructor() {
+    super('TestServer', 3000);
+
+    this.addRoute('/Get', async (request) => {
+      return Promise.resolve({
+        value: 'test',
+      });
+    });
+  }
+}
+
+const test = new TestServer();
+test.listen();
