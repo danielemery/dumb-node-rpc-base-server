dffaa1b2d5b5e4034e5a015d92a9365f1b3f2e98
Daniel Emery <danielremery@gmail.com>
2020-11-04 22:55:09 +1030

    Apply array parseDates fix to server

--- src/middlewares/dateParser.middleware.ts
+++ src/middlewares/dateParser.middleware.ts
@@ -1,7 +1,9 @@
 const dateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/;
 
 function parseDates(part: any): any {
-  if (typeof part === 'object' && part !== null) {
+  if (Array.isArray(part)) {
+    return part.map(parseDates);
+  } else if (typeof part === 'object' && part !== null) {
     return Object.keys(part).reduce((acc, key) => {
       return {
         ...acc,
