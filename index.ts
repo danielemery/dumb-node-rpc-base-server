7634a06551b0680fadd55bb1919154837e221d6e
Daniel Emery <danielremery@gmail.com>
2020-11-07 17:54:01 +1030

    Added options for parseDates

--- index.ts
+++ index.ts
@@ -10,19 +10,30 @@ export interface IVersionInfo {
   [key: string]: string;
 }
 
+export interface IBaseServerOptions {
+  parseDates: boolean;
+}
+
 export default abstract class DumbNodeRPCBaseServer {
   private koa: Koa;
   private router: Router;
   private logger: Logger;
   private port: number;
   private versionInfo: IVersionInfo;
+  options: IBaseServerOptions;
 
-  constructor(loggerName: string, port: number, versionInfo: IVersionInfo) {
+  constructor(
+    loggerName: string,
+    port: number,
+    versionInfo: IVersionInfo,
+    options: IBaseServerOptions = { parseDates: true },
+  ) {
     this.koa = new Koa();
     this.router = new Router();
     this.logger = new Logger(loggerName);
     this.port = port;
     this.versionInfo = versionInfo;
+    this.options = options;
   }
 
   protected addRoute(route: string, service: (request: any) => Promise<any>) {
@@ -36,12 +47,15 @@ export default abstract class DumbNodeRPCBaseServer {
 
   public listen() {
     this.koa.use(bodyParser());
-    this.koa.use(dateParser);
+
+    if (this.options.parseDates) {
+      this.koa.use(dateParser);
+    }
 
     this.router.get(VERSION_ROUTE, (ctx) => {
       ctx.response.status = 200;
       ctx.response.body = this.versionInfo;
-    })
+    });
     this.koa.use(this.router.routes());
 
     this.logger.info(`Application listening on ${this.port}`);
